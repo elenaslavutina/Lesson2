37183f4b4d00782a05da4f3620298fc15c0a235b
elenaslavutina <elena.slavutina2013@gmail.com>
2021-02-12 19:19:30 +0300

    done 6tasks

--- task6.py
+++ task6.py
@@ -5,9 +5,6 @@ list1 = []
 list2 = []
 list3 = []
 list4 = []
-
-
-
 while True:
    info = input("Введите информацию о товарах через пробел : название цена количество единица измерения (напримиемр шт). Для окончания введите выход : ")
    if info == "выход":
@@ -16,16 +13,24 @@ while True:
        list = info.split()
        spisok.append((i,{"Название": list[0],"Цена": int(list[1]),"Количество": int(list[2]),"Единица измерения": list[3]}))
        i+=1
-print(spisok)
-dict2 = {}
+
 for element in spisok:
     elem = element[1]
     print(elem)
-    for key in elem.keys():
-       print(key)
+    my_keys = elem.keys()
+    print(my_keys)
 
-       dict2.update(key,elem[key])
-       print(dict2)
+    if elem.get('Название') not in list1:
+        list1.append(elem.get('Название'))
+    list2.append(elem.get('Цена'))
+    list3.append(elem.get('Количество'))
+    if elem.get('Единица измерения') not in list4:
+        list4.append(elem.get('Единица измерения'))
 
-    #dict2.update(elem)
-#print(dict2)
+print(list1)
+print(list2)
+print(list3)
+print(list4)
+dict2 = {"Название":list1,"цена":list2, "Количество":list3, "Единица измерения":list4}
+print(dict2)
+
